8905dffa5d122afd5d85e1d1c5ed860a6205a175
Mikeintellect29 <Mikeintellect29@gmail.com>
2023-09-15 17:46:24 -0700

    Write a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main"
+/**
+ * mul - Check a function that mutiplies two integers
+ * @x: The first number is multiply
+ * @y: The second number is multiply with first number
+ * Return: The result of multiplication between x paran and y paran
+ */
+
+int mul(int x, int y)
+{
+return (x * y);
+}
