deddeb6476c16ef1b381d37b2b901a2f06c857ac
Mikeintellect29 <Mikeintellect29@gmail.com>
2023-11-21 03:27:57 -0800

    we keep trying

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: count the number of argument
+ * @argv: list the number of array  argument
+ * return:return (0) when successful
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
